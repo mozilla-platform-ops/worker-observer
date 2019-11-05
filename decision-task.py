36ea3cb561f378750de4d0545ec9da8e1d9a72ec
Rob Thijssen <rthijssen@gmail.com>
2019-11-05 10:40:02 +0200

    debug missing version data

--- decision-task.py
+++ decision-task.py
@@ -49,7 +49,7 @@ async def print_task_artifacts(provisioner, workerType, taskGroupId, taskNamespa
   taskStatus = await create_task(provisioner, workerType, taskGroupId, task, iteration, iterations)
   print('{}/{} - {} ({}/{} {}): {}'.format(provisioner, workerType, taskNamespace, iteration, iterations, taskStatus['status']['taskId'], taskStatus['status']['state']))
   while taskStatus['status']['state'] not in ['completed', 'failed']:
-    time.sleep(10 if taskStatus['status']['state'] == 'pending' else 2)
+    time.sleep(2)
     print('{}/{} - {} ({}/{} {}): {}'.format(provisioner, workerType, taskNamespace, iteration, iterations, taskStatus['status']['taskId'], taskStatus['status']['state']))
     taskStatus = await asyncQueue.status(taskStatus['status']['taskId'])
   print('{}/{} - {} ({}/{} {}): {} on run {}'.format(provisioner, workerType, taskNamespace, iteration, iterations, taskStatus['status']['taskId'], taskStatus['status']['state'], taskStatus['status']['runs'][-1]['runId']))  
